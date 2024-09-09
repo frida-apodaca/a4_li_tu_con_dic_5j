8691a5c6b48d820449cf20e460d5c910f715b712
frida-apodaca <a.22308051281136@cbtis128.edu.mx>
2024-09-09 10:12:35 -0600

    Add files via upload

---
+++ mi_tuplas.py
@@ -0,0 +1,9 @@
+print("ejemplo de tuplas")
+canciones=("intro","maybe","13","season","wave to earth")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "yuno vs lucy"
+x = tuple(y)
+
+print(x)
