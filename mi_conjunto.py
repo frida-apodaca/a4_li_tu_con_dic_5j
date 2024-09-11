628c508c971d4a7d06818117db5c2ef665c67028
frida-apodaca <a.22308051281136@cbtis128.edu.mx>
2024-09-11 07:49:53 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("ejemplo de conjuntos set")
+tipodedeportes={"soccer","voleyball","softball","basketball"}
+print(tipodedeportes)
+print("Lista de elementos de conjunto tipodeportes loop for")
+print(tipodedeportes)
+for apodaca in tipodedeportes:
+    print(apodaca)
---
+++ mi_diccionario.py
@@ -0,0 +1,10 @@
+print("ejemplo de diccionario")
+album={"ate","sin miedo","lamp genso","who really cares","golden hour: part.1","ate that ep"}
+thisdict = {
+  "ate": "stray kids",
+  "lamp genso": "lamp",
+  "ate that ep": "young posse"
+}
+print(thisdict)
+for x in thisdict:
+  print(x)
